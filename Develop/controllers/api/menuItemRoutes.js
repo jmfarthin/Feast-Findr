2d893c52da68f83f6856bb2590d92a08d2a44ecf
nnamdionyeije <81874532+nnamdionyeije@users.noreply.github.com>
2023-05-02 20:45:27 -0400

    Made a few small changes to the api routes
    added some code to the food truck and menu item routes

--- Develop/controllers/api/menuItemRoutes.js
+++ Develop/controllers/api/menuItemRoutes.js
@@ -7,12 +7,33 @@ router.post('/', withAuth, async (req, res) => {
     try {
         const itemData = await MenuItem.create({
 
-            ...req.body,
-            user_id: req.session.user_id,
+            ...req.body // the food truck ID will be passed in through the front end when it is called
         });
 
-        
+        res.status(200).json(itemData);
     } catch (err) {
         res.status(400).json(err);
     }
 });
+
+
+// A route to delete a menu item
+router.delete('/', withAuth, async (req, res) => {
+    try {
+        const itemData = await MenuItem.destroy({
+            where: {
+                id: req.params.id,
+                // food_truck_id: 
+            },
+        });
+
+        if (!itemData) {
+            res.status(404).json({ message: 'No menu item found with this id!' });
+            return;
+        }
+
+        res.status(200).json(itemData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
