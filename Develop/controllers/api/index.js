55f35160aaf9d08e99bd4f6b4a288dd5ba2e0a04
nnamdionyeije <81874532+nnamdionyeije@users.noreply.github.com>
2023-05-04 20:10:35 -0400

    Added some res.render routes and fixed the navbar

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const commentRoutes = require('./commentRoutes');
-const foodTruckRoutes = require('./foodTruckRoutes');
+// const commentRoutes = require('./commentRoutes');
+// const foodTruckRoutes = require('./foodTruckRoutes');
 
 router.use('/users', userRoutes);
-router.use('/comments', commentRoutes);
-router.use('/foodTrucks', foodTruckRoutes);
+// router.use('/comments', commentRoutes);
+// router.use('/foodTrucks', foodTruckRoutes);
 
 module.exports = router;
