546a92cf8286bb7820315935f4af9cf2298405a7
jmfarthin <122315957+jmfarthin@users.noreply.github.com>
2023-05-07 19:25:35 -0400

    added checkDistance function

--- Develop/controllers/homeRoutes.js
+++ Develop/controllers/homeRoutes.js
@@ -47,10 +47,6 @@ router.get('/login', (req, res) => {
     })
 });
 
-<<<<<<< HEAD
-// A route that will allow the user to find the closest nearby food trucks with Opencage, passing it through geolib
-router.post('/search', searchRoute.findNearbyFoodTrucks);
-=======
 router.get('/truck', (req, res) => {
     res.render('truck');
 });
@@ -60,7 +56,6 @@ router.get('/menu', (req, res) => {
 })
 
 module.exports = router;
->>>>>>> 9ca8dd4a89fe5e3bbfdcc75910c1cd0ad7c78573
 
 
 // A route to get the user's coordinates 
--- Develop/utils/geolocation.js
+++ Develop/utils/geolocation.js
@@ -1,21 +1,24 @@
 // This will import the fetch method
 
 const fetch = require('node-fetch');
+const geolib = require('geolib');
 
 //this formula takes the address and passes it into the api parameters to geocode the string address and return the resulting coordinates
 const convertAddressToCoordinates = async (address) => {
-  const apiKey = process.env.e67eaf040010438d8c6b339939fdd6ea;
+  const apiKey = 'e67eaf040010438d8c6b339939fdd6ea';
   const url = `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(address)}&key=${apiKey}`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
-    
+
     if (data.results.length === 0) {
       throw new Error('No coordinates found for the provided address');
     }
 
     const { lat, lng } = data.results[0].geometry;
+
+    console.log(lat, lng)
     return { latitude: lat, longitude: lng };
   } catch (error) {
     console.error('Error converting address to coordinates:', error.message);
@@ -26,3 +29,17 @@ const convertAddressToCoordinates = async (address) => {
 module.exports = {
   convertAddressToCoordinates,
 };
+
+const location1 = '490 Roaring fork lake rd';
+const location2 = '2240 Burton Lake Rd'
+
+
+const checkDistance = async (first, second) => {
+  const home = await convertAddressToCoordinates(first)
+  const foodTruck = await (convertAddressToCoordinates(second));
+  const distance = geolib.getDistance(home, foodTruck);
+  const mileDistance = distance * 0.000621;
+  console.log(mileDistance);
+  return mileDistance;
+}
+checkDistance(location1, location2);
