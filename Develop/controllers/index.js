343375134378da866003297d92c98ee4df19343d
jmfarthin <122315957+jmfarthin@users.noreply.github.com>
2023-05-08 20:28:42 -0400

    fixed search results functionality

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -2,9 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes.js');
+const searchRoute = require('./searchRoute')
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-
+router.use('/results', searchRoute);
 
 module.exports = router;
